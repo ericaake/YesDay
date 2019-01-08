73741a29a6ad8fecfcf7b476a16a88eed4dec2de
Emelie Andersson <emelieandersson.ea@gmail.com>
2019-01-08 16:49:30 +0100

    Added Identity

--- YesDay/Startup.cs
+++ YesDay/Startup.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using YesDay.Models;
 
 namespace YesDay
 {
@@ -15,20 +20,33 @@ namespace YesDay
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=YesDayLocalDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            services.AddDbContext<MyIdentityContext>(o => o.UseSqlServer(connString));
+            services.AddIdentity<MyIdentityUser, IdentityRole>(o =>
+                    {
+                        o.Password.RequireNonAlphanumeric = false;
+                        o.Password.RequiredLength = 8;
+                    })
+                    .AddEntityFrameworkStores<MyIdentityContext>()
+                    .AddDefaultTokenProviders();
+            services.ConfigureApplicationCookie(o => o.LoginPath = "/couple/login");
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(o => o.LoginPath = "/couple/login");
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //app.UseSession();
+            app.UseAuthentication();
+            //var mySecretConnString = conf.GetConnectionString("myConnString");
+            app.UseDeveloperExceptionPage();
+            app.UseMvcWithDefaultRoute();
+            //app.UseStaticFiles();
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello WORLD");
+            //});
         }
     }
 }
