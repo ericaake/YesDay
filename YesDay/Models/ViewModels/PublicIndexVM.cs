1c7b2f6d9b6e8a6b2adbd418fd137d16e8515048
Annelie Irmalm <annelie.irmalm@gmail.com>
2019-01-09 14:31:09 +0100

    added layoutpages and viewimport/start

---
+++ YesDay/Models/ViewModels/PublicIndexVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YesDay.Models.ViewModels
+{
+    public class PublicIndexVM
+    {
+    }
+}
